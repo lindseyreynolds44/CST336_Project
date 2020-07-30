157530887ba21d1dbe2232077c02a7518300917e
Dan Sedano <dandano2006@gmail.com>
2020-07-30 14:34:29 -0700

    made some organizational changes. made requestUrl into an object for readability, and organized code to pull API key from .env file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # CST336_Project
+
+# Team — PLEASE READ FIRST
+
+Be sure to run `npm i` before starting work to install all dependencies
+
+## Working on project locally instructions:
+
+- Create a `.env` file at the same level as app.js file
+- Add the following variables:
+
+```
+PORT=8080
+IP=localhost
+API_KEY="add the api key here"
+```
+
+- Save the file
+- Run using nodemon you should see the api key print out in the console. I added console.log for diagnostic purposes
+
+## Working on project using cloud 9
+
+- Create a `.env` file at the same level as app.js file
+- Add the following variable:
+
+```
+API_KEY="add the api key here"
+```
+
+- Save the file
+- Run using nodemon you should see the api key print out in the console. I added console.log for diagnostic purposes
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,39 +1,45 @@
 const request = require("request");
 const pool = require("../dbPool.js");
-const key = "2e9106137e566a1c862c47d7251bb5fe";
+const API_KEY = process.env.API_KEY;
 
 exports.displayIndex = async (req, res) => {
-    let query = "Jack Reacher";
-    let resultArray = await getMovieID(query);
-    res.render("index", {"resultArray": resultArray});
+  let query = "Jack Reacher";
+  let resultArray = await getMovieID(query);
+  res.render("index", { resultArray: resultArray });
 };
 
-function getMovieID(query){
-    return new Promise (function (resolve, reject){
-       let requestUrl = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${query}`;
-    
-        request(requestUrl, function(error, response, body){ //body is the string that is retrieved 
-            //check that the request was successful 
-            if(!error && response.statusCode == 200){
-                let parsedData = JSON.parse(body);
-                let numResults = parsedData.total_results;
-                let resultArray = [];
-                for(let i = 0; i < numResults; i++){
-                    let result = {
-                        "title": parsedData.results[i].original_title,
-                        "movieID": parsedData.results[i].id
-                    }
-                    resultArray.push(result);
-                }
-                resolve(resultArray);
-            }
-            else {
-                console.log('error:', error);
-                console.log('statusCode:', response && response.statusCode);
-                reject(error);
-            }
-        });
+function getMovieID(query) {
+  return new Promise(function (resolve, reject) {
+    // reorganized to make the url easier to manipulate and read
+    // qs adds the query string after the url.
+    let requestUrl = {
+      url: "https://api.themoviedb.org/3/search/movie",
+      qs: {
+        api_key: API_KEY,
+        query: query,
+      },
+    };
+
+    request(requestUrl, function (error, response, body) {
+      //body is the string that is retrieved
+      //check that the request was successful
+      if (!error && response.statusCode == 200) {
+        let parsedData = JSON.parse(body);
+        let numResults = parsedData.total_results;
+        let resultArray = [];
+        for (let i = 0; i < numResults; i++) {
+          let result = {
+            title: parsedData.results[i].original_title,
+            movieID: parsedData.results[i].id,
+          };
+          resultArray.push(result);
+        }
+        resolve(resultArray);
+      } else {
+        console.log("error:", error);
+        console.log("statusCode:", response && response.statusCode);
+        reject(error);
+      }
     });
+  });
 }
-
-
