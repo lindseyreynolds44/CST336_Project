1583a876d23bca35342890ef81ea3ccf52ab2f42
EC2 Default User <ec2-user@ip-172-31-18-107.us-west-1.compute.internal>
2020-08-02 22:49:29 +0000

    added function headers

---
+++ public/js/functions.js
@@ -0,0 +1,32 @@
+/* global $ */
+$(document).ready(function() {
+
+/**
+ * Search button action event
+ */
+ // Request this movie using an AJAX call to route: *** FILL IN ***
+
+ 
+ /**
+ * Create new account action event
+ */
+ // Add new user and password using AJAX to route: *** FILL IN ***
+ 
+ /** 
+ * User clicks "login" action event
+ */
+ 
+ /**
+ * User clicks "logout" action event
+ */
+ 
+ /**
+ * Add movie to Cart action event
+ */
+ 
+ /**
+ * Remove movie from Cart action event
+ */
+  
+    
+});
